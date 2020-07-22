0b3b5ec3d6d8a72b65f75b061df12ac2bcf04ee1
aneesh anil <aneeshanil008@gmail.com>
2020-07-22 22:30:46 +0530

    first commit

---
+++ src/main/java/should/work/with/any/ide/GitUploadWorksWithAnyIdeApplication.java
@@ -0,0 +1,13 @@
+package should.work.with.any.ide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUploadWorksWithAnyIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUploadWorksWithAnyIdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/should/work/with/any/ide/GitUploadWorksWithAnyIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package should.work.with.any.ide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitUploadWorksWithAnyIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
